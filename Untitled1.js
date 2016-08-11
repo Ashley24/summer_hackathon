63a78d7df91a447d31617ada2e59b7a1b3a0b25b
samueljean <samueljean09@gmail.com>
2016-08-11 13:58:53 +0000

    Intro to game added to Javascript

---
+++ Untitled1.js
@@ -0,0 +1,5 @@
+// JavaScript File
+var c=document.getElementById("mycanvas");
+var ctx=c.getContext("2d");
+ctx.font="20px Georgia";
+ctx.fillText("Hello World!",10,50);
---
+++ intro.js
@@ -0,0 +1,13 @@
+// JavaScript File
+$(document).ready(
+function (){
+//start button show intro screen
+$("#buttonStart").click(function(){
+    $("#StartMenu").hide();
+    $("#Intro").show();
+})
+//next button show game
+$("#buttonIntro").click(function(){
+    $("#Intro").hide();
+    window.location="index2.html"
+})
